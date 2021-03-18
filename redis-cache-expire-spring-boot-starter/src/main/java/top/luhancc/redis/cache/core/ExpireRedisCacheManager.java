513ebcb2891db979e0b3371d318fec66aa4cd686
luhan <765478939@qq.com>
2021-03-18 13:48:07 +0800

    优化cache name相同只创建一个Cache

--- redis-cache-expire-spring-boot-starter/src/main/java/top/luhancc/redis/cache/core/ExpireRedisCacheManager.java
+++ redis-cache-expire-spring-boot-starter/src/main/java/top/luhancc/redis/cache/core/ExpireRedisCacheManager.java
@@ -14,6 +14,8 @@ import top.luhancc.redis.cache.annotation.CacheExpire;
 
 import java.lang.reflect.Method;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 实现自定义缓存过期时间的CacheManager
@@ -26,6 +28,8 @@ public class ExpireRedisCacheManager extends RedisCacheManager {
     private final Logger log = LoggerFactory.getLogger(ExpireRedisCacheManager.class);
     private final RedisCacheConfiguration defaultCacheConfiguration;
 
+    private Map<String, Cache> singletonCacheObjects = new HashMap<>();
+
     public ExpireRedisCacheManager(RedisCacheWriter cacheWriter,
                                    RedisCacheConfiguration defaultCacheConfiguration) {
         super(cacheWriter, defaultCacheConfiguration);
@@ -34,18 +38,39 @@ public class ExpireRedisCacheManager extends RedisCacheManager {
 
     @Override
     public Cache getCache(String name) {
-        Cache cache = super.getCache(name);
+        if (!singletonCacheObjects.containsKey(name)) {
+            Cache cache = super.getCache(name);
+
+            // 如果需要过期时间  就将cache进行包装下
+            if (needExpire()) {
+                cache = wrapExpire(cache);
+            }
+            singletonCacheObjects.put(name, cache);
+            return cache;
+        }
+        return singletonCacheObjects.get(name);
+    }
+
+    private boolean needExpire() {
         CacheOperationInvocationContext<?> context = CacheOperationInvocationContextHolder.context();
         Method targetMethod = ReflectionUtils.findMethod(context.getTarget().getClass(), context.getMethod().getName());
-        if (targetMethod.isAnnotationPresent(CacheExpire.class)) {
-            CacheExpire cacheExpire = AnnotationUtils.getAnnotation(targetMethod, CacheExpire.class);
-            if (cache instanceof RedisCache) {
-                RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                        .entryTtl(Duration.ofSeconds(cacheExpire.expire()))
-                        .serializeKeysWith(defaultCacheConfiguration.getKeySerializationPair())
-                        .serializeValuesWith(defaultCacheConfiguration.getValueSerializationPair());
-                return createRedisCache(name, cacheConfiguration);
-            }
+        return targetMethod != null && targetMethod.isAnnotationPresent(CacheExpire.class);
+    }
+
+    private Cache wrapExpire(Cache cache) {
+        CacheOperationInvocationContext<?> context = CacheOperationInvocationContextHolder.context();
+        Method targetMethod = ReflectionUtils.findMethod(context.getTarget().getClass(), context.getMethod().getName());
+        if (targetMethod == null) {
+            return cache;
+        }
+        CacheExpire cacheExpire = AnnotationUtils.getAnnotation(targetMethod, CacheExpire.class);
+        if (cache instanceof RedisCache) {
+            RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                    .entryTtl(Duration.ofSeconds(cacheExpire.expire()))
+                    .serializeKeysWith(defaultCacheConfiguration.getKeySerializationPair())
+                    .serializeValuesWith(defaultCacheConfiguration.getValueSerializationPair());
+            log.info("对{}进行过期时间[{}]包装", cache, cacheConfiguration.getTtl());
+            return createRedisCache(cache.getName(), cacheConfiguration);
         }
         return cache;
     }
